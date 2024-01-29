c6cbebaed6da6b828adfc1832ffb2ad7b68a24e0
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2024-01-29 23:42:06 +0600

    React Router Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { Container, Grid, Typography } from "@mui/material";
 import "./App.css";
-import TourCard from "./components/TourCard";
-import SearchAppBar from "./components/AppBar";
-import cities from "./data.json";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import TourPage from "./pages/TourPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/:id",
+    element: <TourPage />,
+  },
+]);
 
 function App() {
-  return (
-    <div>
-      <SearchAppBar />
-      <Container sx={{ marginY: 5 }}>
-        {cities.map((city) => (
-          <>
-            <Typography
-              variant="h4"
-              component="h2"
-              marginBottom={3}
-              marginTop={5}
-            >
-              Top {city.name} Tours
-            </Typography>
-            <Grid container spacing={5}>
-              {city.tours.map((tour) => (
-                <TourCard tour={tour} />
-              ))}
-            </Grid>
-          </>
-        ))}
-      </Container>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
