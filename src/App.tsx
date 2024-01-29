257fe87cc0a2cec86042b356a34175db940ead63
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2024-01-29 21:15:03 +0600

    grid cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import "./App.css";
 import TourCard from "./components/TourCard";
+import SearchAppBar from "./components/AppBar";
+import cities from "./data.json";
 
 function App() {
   return (
     <div>
-      <Container>
-        <Grid container spacing={5}>
-          <TourCard />
-          <TourCard />
-          <TourCard />
-          <TourCard />
-        </Grid>
+      <SearchAppBar />
+      <Container sx={{ marginY: 5 }}>
+        {cities.map((city) => (
+          <>
+            <Typography
+              variant="h4"
+              component="h2"
+              marginBottom={3}
+              marginTop={5}
+            >
+              Top {city.name} Tours
+            </Typography>
+            <Grid container spacing={5}>
+              {city.tours.map((tour) => (
+                <TourCard tour={tour} />
+              ))}
+            </Grid>
+          </>
+        ))}
       </Container>
     </div>
   );
--- src/components/TourCard.tsx
+++ src/components/TourCard.tsx
@@ -1,15 +1,17 @@
 import { AccessTime } from "@mui/icons-material";
-import {
-  Box,
-  Grid,
-  Paper,
-  Rating,
-  ThemeProvider,
-  Typography,
-  createTheme,
-} from "@mui/material";
+import { Box, Grid, Paper, Rating, Typography } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-const TourCard = () => {
+interface Props {
+  tour: {
+    name: string;
+    price: number;
+    rating: number;
+    image: string;
+  };
+}
+
+const TourCard: React.FC<Props> = ({ tour }) => {
   const theme = createTheme({
     components: {
       MuiTypography: {
@@ -39,14 +41,10 @@ const TourCard = () => {
     <Grid item xs={3}>
       <ThemeProvider theme={theme}>
         <Paper elevation={3}>
-          <img
-            src="https://cdn.britannica.com/22/154122-050-B1D0A7FD/Skyline-Los-Angeles-California.jpg"
-            alt="image"
-            className="img"
-          />
+          <img src={tour.image} alt="image" className="img" />
           <Box paddingX={1}>
             <Typography variant="subtitle1" component="h2">
-              Los Angeles
+              {tour.name}
             </Typography>
             <Box
               sx={{
@@ -65,7 +63,12 @@ const TourCard = () => {
                 alignItems: "center",
               }}
             >
-              <Rating value={4.5} readOnly precision={0.5} size="small" />
+              <Rating
+                value={tour.rating}
+                readOnly
+                precision={0.5}
+                size="small"
+              />
               <Typography variant="body2" component="span" marginLeft={1}>
                 4.5
               </Typography>
